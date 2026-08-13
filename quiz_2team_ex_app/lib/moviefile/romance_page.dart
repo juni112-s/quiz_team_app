5f520bc19e853a06dd135e20fa163abd567c1d6a
석준 작업 파일 <rkdtjrwns222@naver.com>
2026-08-13 14:56:53 +0900

    2팀 프로젝트 따라만들기

---
+++ quiz_2team_ex_app/lib/moviefile/romance_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Romancepage extends StatefulWidget {
+  const Romancepage({super.key});
+
+  @override
+  State<Romancepage> createState() => _RomancepageState();
+}
+
+class _RomancepageState extends State<Romancepage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
